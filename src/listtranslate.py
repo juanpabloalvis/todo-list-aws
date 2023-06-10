1f948041c96c8fa0593a9666ff18201d9be374b3
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-06-09 22:05:17 -0500

    translate feature

---
+++ src/listtranslate.py
@@ -0,0 +1,21 @@
+import json
+import decimalencoder
+import todoList
+import boto3
+
+def listtranslate(event, context):
+    translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+
+    # Obtener los parámetros del evento
+    target_language = todoList.get_item(event['pathParameters']['language'])
+    items = todoList.get_items()
+    for item in items:
+        text = item['text']
+        item['text'] = translate.translate_text(Text=text, SourceLanguageCode="en", TargetLanguageCode=target_language)
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(items, cls=decimalencoder.DecimalEncoder)
+    }
+    return response
